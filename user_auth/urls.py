055d58ada14df576f181515941ee7979adfe70d2
Ullas Diwakar <ullasdiwakar96@gmail.com>
2017-02-09 17:15:01 +0530

    Solved the Login issue. Only admins can login

--- user_auth/urls.py
+++ user_auth/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     url(r'login', views.login, name='login'),
     url(r'logout', views.logout, name='logout'),
     url(r'makeadmins', views.create_admin, name='make'),
+    url(r'validate', views.validate, name='validate'),
 ]
--- user_auth/views.py
+++ user_auth/views.py
@@ -9,20 +9,8 @@ from django.views.decorators.csrf import csrf_exempt
 
 @csrf_exempt
 def login(request):
-    if request.method == 'POST':
-        form = LoginForm(request.POST)
-        if form.is_valid():
-            email = form.cleaned_data['email']
-            admin_key = ndb.Key('Admins', email)
-            admin = admin_key.get()
-            if admin is None:
-                msg = "Looks like you are not one of the admins."
-                return render(request, 'login_error.html', {'message' : msg})
-            else:
-                url = users.create_login_url(dest_url=request.GET.get('next'))
-                return HttpResponseRedirect(url)
-    form = LoginForm()
-    return render(request, 'login.html', {'form' : form})
+    url = users.create_login_url('user_auth/validate')
+    return HttpResponseRedirect(url)
 
 def logout(request):
     """Redirects to the homepage after logging the user out."""
@@ -36,20 +24,13 @@ def create_admin(request):
     list_of_keys = ndb.put_multi(entity_list)
     return HttpResponse("The Admins you wanted are created")
 
-
-"""
-if request.method == 'POST':
-        form = LoginForm(request.POST)
-        if form.is_valid():
-            email = form.cleaned_data['email']
-            admin_key = ndb.key('Admins', email)
-            admin = admin_key.get()
-            if admin is None:
-                msg = "Looks like you are not one of the admins."
-                return render(request, 'login_error.html', {'message' : msg})
-            else:
-                url = users.create_login_url(dest_url=request.GET.get('next'))
-                return HttpResponseRedirect(url)
-    form = LoginForm()
-    return render(request, 'login.html', {'form' : form})
-"""
+def validate(request):
+    user = users.GetCurrentUser()
+    email = user.email()
+    admin_key = ndb.Key('Admins', email)
+    admin = admin_key.get()
+    if admin is None:
+        msg = "Sorry, You are not an admin"
+        return render(request, 'login_error.html', {'message' : msg})
+    else:
+        return HttpResponseRedirect('/home')
